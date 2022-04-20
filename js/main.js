2d605559b702e68e56c317321e246f5512f54880
sherzod2110 <sh.azamatugli2110@gmail.com>
2022-04-20 14:13:26 +0500

    add  sdfasdgsd

--- js/main.js
+++ js/main.js
@@ -2,25 +2,33 @@
 let elInp = document.querySelector("#inp");
 let elBtn = document.querySelector(".form_btn");
 let elClen = document.querySelector(".form_clear");
+let elImg = document.querySelector(".form_img");
 let elListt = document.querySelector(".list");
 
 elBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
-
   let firstName = elInp.value.trim();
 
-  if (firstName == "" || firstName.length < 3 || firstName.length > 20) {
+
+  if (firstName == "" || firstName.length < 3 || firstName.length > 15) {
     
     elInp.style.borderColor = "red";
+    elImg.style = "filter: invert(13%) sepia(90%) saturate(6913%) hue-rotate(0deg) brightness(100%) contrast(112%);";
+    placeholder.style.Color = "red";
     return
   }
-  elInp.style.borderColor = "black";
+  elInp.style.borderColor = "#757575";
+  elImg.style = "filter: invert(52%) sepia(4%) saturate(25%) hue-rotate(315deg) brightness(85%) contrast(74%);";
 
   let newLi = document.createElement("li");
   let newP1 = document.createElement("p");
   newP1.textContent = `Product name: ${firstName}`; 
   newLi.append(newP1);
-
   elListt.append(newLi);
 
 })  
+
+elClen.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  elListt.innerHTML = "";
+})
